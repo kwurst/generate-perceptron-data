c161a1dfd7c09abc9167dde1395f4980f9057034
Karl R. Wurst <karl@w-sts.com>
2015-02-22 19:02:48 -0500

    Add recognition data generation.

---
+++ generate-recognition-data.py
@@ -0,0 +1,93 @@
+# Copyright (C) 2015 Karl R. Wurst
+# 
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 3 of the License, or
+# (at your option) any later version.
+# 
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301, USA
+
+####################################################################
+# Generate random character training data for a perceptron
+#
+# The characters are 5x7 (5 bits wide, 7 bits high)
+# turned sideways (90 degrees clockwise) from bitmaps posted
+# by G. Forrest Cook and released under the GPLv3 license.
+# See fonttable.py for bitmaps.
+# 
+# A random number of characters A-Z, and space are generated,
+# and each letter has 0-2 random bits flipped to introduce errors.
+
+import random
+from fonttable import fonttable
+
+zero = '0'
+one = '1'
+minErrors = 0
+maxErrors = 2
+
+def flipBit(bitPattern, position):
+    if bitPattern[position] == zero:
+        bitPattern[position] = one
+    else:
+        bitPattern[position] = zero
+        
+    return bitPattern
+    
+def introduceErrors(bitPattern):
+    numErrors = random.randint(minErrors,maxErrors)
+    
+    for error in range(numErrors):
+        errorPosition = random.randint(0,len(bitPattern)-1)
+        bitPattern = flipBit(bitPattern, errorPosition)
+    
+    return bitPattern  
+
+def getBitPattern(asciiVal):
+    bitPattern = []
+    firstcol = (asciiVal - ord(' ')) * 5
+
+    for col in range (0, 5):
+        line = fonttable[firstcol + col]
+        
+        for row in [1, 2, 4, 8, 16, 32, 64]:
+            if (line & row): 
+                bitPattern.append(one)
+            else: 
+                bitPattern.append(zero)
+
+    return bitPattern
+
+def printSideways(bitPattern):
+    print(bitPattern[-1])
+    print(bitPattern[0:7])
+    print(bitPattern[7:14])
+    print(bitPattern[14:21])
+    print(bitPattern[21:28])
+    print(bitPattern[28:35])
+
+# Main program starts here
+
+fin = open('gettysburg.txt', 'r')
+fout=open('recognition.dat', 'w')
+
+text = fin.read()
+for char in text:
+    if char not in ' ABCDEFGHIJKLMNOPQRSTUVWXYZ':
+        char = ' '
+    
+    bitPattern = getBitPattern(ord(char))
+    bitPattern = introduceErrors(bitPattern)
+
+    bitString = ''.join(bitPattern)
+
+    fout.write(bitString+'\n')
+    
+fout.close()
